8df2694d99928577cf1f637d29b5a76d4a8af592
James Liu <jamesliu96@163.com>
2014-11-14 09:12:36 +0800

    Update form part

--- m.js
+++ m.js
@@ -436,7 +436,7 @@ M.alert = function(c) {
 };
 M.form = function(c) {
     c = c ? c : {};
-    id = c.id || "form";
+    var id = c.id || "form";
     jQuery(id).attr("novalidate", "");
     var whiteList = [
         "这个邮箱还没有注册哦",
@@ -483,7 +483,9 @@ M.form = function(c) {
         }
     };
     var showAlert = function(o, content) {
-        if (jQuery(".popover-content:visible").length !== 0) return;
+        if (jQuery(".popover-content:visible").length !== 0 && !jQuery(".popover-content:visible").closest(".popover").prev().is(o)) {
+            return;
+        }
         if (o.data("content") !== content) {
             o.popover("destroy");
             o.focus().data("content", content).popover("show");
@@ -519,7 +521,11 @@ M.form = function(c) {
     });
     jQuery(id).submit(function(e) {
         e.preventDefault();
-        if (jQuery(".popover-content:visible").length !== 0) return;
+        jQuery(this).find("input, textarea").trigger("input");
+        if (jQuery(".popover-content:visible").length !== 0) {
+            jQuery(".popover-content:visible").closest(".popover").prev().focus();
+            return;
+        }
         var no_empty = true;
         var sendData = {};
         jQuery(this).find("input, textarea").each(function() {
